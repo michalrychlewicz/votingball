844b2d84f01a8e6cf0430f5e29f02341bfed2476
Michal Rychlewicz <michal.rychlewicz@nokia.com>
2019-02-05 11:02:11 +0100

    Refactored test

--- src/test/java/com/votingball/votingball/dao/PollsRepositoryIntegrationTest.java
+++ src/test/java/com/votingball/votingball/dao/PollsRepositoryIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.votingball.votingball.dao;
 
-import com.mysql.cj.exceptions.AssertionFailedException;
 import com.votingball.votingball.entity.Poll;
 import com.votingball.votingball.entity.Position;
 import org.junit.Test;
@@ -37,17 +36,14 @@ public class PollsRepositoryIntegrationTest
     @Test
     public void whenFindById_thenReturnPoll() {
         // given
-        Poll pollBeingInserted = preparePollForInsertion();
+        Poll pollBeingInserted = preparePollForInsertion("pollTitle");
         Poll persistedPoll = entityManager.persist(pollBeingInserted);
         entityManager.flush();
         int pollId = persistedPoll.getId();
 
         // when
         Optional<Poll> retrievedPoll = employeeRepository.findById(pollId);
-        if(!retrievedPoll.isPresent())
-        {
-            throw new AssertionFailedException("Poll was just persisted. It has to be present in database.");
-        }
+        assertThat("Poll was just deleted. It can not exist in database",retrievedPoll.isPresent(),is(true));
 
         // then
         Poll actualPoll = retrievedPoll.get();
@@ -55,29 +51,34 @@ public class PollsRepositoryIntegrationTest
     }
 
     @Test
-    public void whenDeletePollFromDatabase_thenDatabaseShouldNotContainThatPollAndItsPositions() {
+    public void whenDeletePollFromDatabase_thenDatabaseShouldNotContainThatPollAndItsPositionsAndOtherOnesAreUntouched() {
         // given
-        Poll pollBeingInserted = preparePollForInsertion();
+        Poll pollBeingInserted = preparePollForInsertion("pollTitle");
         Poll persistedPoll = entityManager.persist(pollBeingInserted);
+        Poll otherPoll = preparePollForInsertion("pollTittle2");
+        Poll persistedOtherPoll = entityManager.persist(otherPoll);
         entityManager.flush();
         int pollId = persistedPoll.getId();
+        int otherPollId = persistedOtherPoll.getId();
         Set<Position> persistedPollPositions = persistedPoll.getPositions();
+        List<Position> persistedOtherPollPositions = new ArrayList<>(persistedOtherPoll.getPositions());
 
         // when
         employeeRepository.delete(persistedPoll);
 
         Optional<Poll> retrievedPoll = employeeRepository.findById(pollId);
         List<Position> retrievedPositions = getAllExistingCorrespondingPositionsByTheirIds(persistedPollPositions);
+        Optional<Poll> retrievedOtherPoll = employeeRepository.findById(otherPollId);
+        List<Position> retrievedOtherPositions = getAllExistingCorrespondingPositionsByTheirIds(persistedOtherPollPositions);
 
         // then
-        if(retrievedPoll.isPresent())
-        {
-            throw new AssertionFailedException("Poll was just deleted. It can not exist in database");
-        }
+        assertThat("Poll was just deleted. It can not exist in database",retrievedPoll.isPresent(),is(false));
         assertThat("Positions should be cascade removed",retrievedPositions.isEmpty(),is(true));
+        assertThat(retrievedOtherPoll.get(),is(equalTo(persistedOtherPoll)));
+        assertThat(retrievedOtherPositions,equalTo(persistedOtherPollPositions));
     }
     
-    private List<Position> getAllExistingCorrespondingPositionsByTheirIds(Set<Position> persistedPollPositions) {
+    private List<Position> getAllExistingCorrespondingPositionsByTheirIds(Collection<Position> persistedPollPositions) {
         List<Position> retrievedPositions = new ArrayList<>();
         for(Position persistedPosition : persistedPollPositions)
         {
@@ -90,14 +91,13 @@ public class PollsRepositoryIntegrationTest
         return retrievedPositions;
     }
 
-    private Poll preparePollForInsertion() {
-        String title = "Poll title";
+    private Poll preparePollForInsertion(String pollTittle) {
         LocalDateTime createdAt = LocalDateTime.of(2019,02,04,10,30);
         LocalDateTime modifiedAt = LocalDateTime.of(2019,02,04,10,30);
         Position position1 = new Position(1,"First position",0);
         Position position2 = new Position(2,"Second position",3);
-        Set<Position> positions = new HashSet<>(Arrays.asList(position1,position2));
+        Set<Position> positions = new TreeSet<>(Arrays.asList(position1,position2));
 
-        return new Poll(title,createdAt,modifiedAt,positions);
+        return new Poll(pollTittle,createdAt,modifiedAt,positions);
     }
 }
